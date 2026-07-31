d1ebe5d959b7974d238270a0b1ccf673eb32b21c
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-08-01 00:41:32 +0530

    Implemented the First Task with Buffer Concept

--- NodeJS-Stream_Task1/app.js
+++ NodeJS-Stream_Task1/app.js
@@ -11,26 +11,56 @@ const server = http.createServer((req, res) => {
     });
 
     res.end(`
-            <h1>Home Page</h1>
+      <h1>Home Page</h1>
 
-            <form action="/message" method="POST">
-                <label>Name : </label>
-                <input type="text" name="username">
-                <button type="submit">Add</button>
-            </form>
-        `);
+      <form action="/message" method="POST">
+          <label>Name : </label>
+          <input type="text" name="username">
+          <button type="submit">Add</button>
+      </form>
+
+      <br>
+      <a href="/read">Read Messages</a>
+    `);
   }
 
   // Save Message
   else if (url.pathname === "/message" && req.method === "POST") {
-    let body = "";
+    // Array to store Buffer chunks
+    const chunks = [];
 
     req.on("data", (chunk) => {
-      body += chunk;
+      console.log("--------------------------------");
+      console.log("Received Chunk:");
+      console.log(chunk);
+      console.log("Is Buffer :", Buffer.isBuffer(chunk));
+      console.log("Chunk Size:", chunk.length, "bytes");
+
+      // Store the Buffer
+      chunks.push(chunk);
     });
 
     req.on("end", () => {
-      const username = body.split("=")[1];
+      console.log("--------------------------------");
+      console.log("All Chunks Received");
+
+      // Combine all Buffers
+      const fullBuffer = Buffer.concat(chunks);
+
+      console.log("Combined Buffer:");
+      console.log(fullBuffer);
+
+      // Convert Buffer into String
+      const body = fullBuffer.toString();
+
+      console.log("Body String:");
+      console.log(body);
+
+      // Parse the form data
+      const params = new URLSearchParams(body);
+      const username = params.get("username");
+
+      console.log("Username:", username);
 
       fs.writeFile("messages.txt", username, (err) => {
         if (err) {
@@ -38,25 +68,36 @@ const server = http.createServer((req, res) => {
           return res.end("Error writing file");
         }
 
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
+        res.writeHead(302, {
+          Location: "/",
+        });
+
         res.end();
       });
     });
-  } else if(url.pathname === "/read"){
+  }
+
+  // Read Message
+  else if (url.pathname === "/read") {
     fs.readFile("messages.txt", "utf8", (err, data) => {
       if (err) {
         res.writeHead(500);
         return res.end("Error reading file");
       }
+
       res.writeHead(200, {
-        "Content-Type": "text/html"
+        "Content-Type": "text/html",
       });
-      res.end(`<h1>Messages</h1><p>${data}</p>`);
+
+      res.end(`
+        <h1>Messages</h1>
+        <p>${data}</p>
+        <a href="/">Go Back</a>
+      `);
     });
   }
-  
-  
+
+  // 404
   else {
     res.writeHead(404, {
       "Content-Type": "text/html",
