d4c6fefa26dad60def75c55fda1bff1c34146147
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-08-09 21:21:31 +0530

    Added Code for Server Creation in Express

---
+++ Express-WebServer/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+let app = express();
+
+app.get('/orders', (req, res) => {
+    res.send('Here is the list of all orders.');
+});
+
+app.post('/orders', (req, res) => {
+    res.send('A new order has been created.');
+});
+
+app.get('/users', (req, res) => {
+    res.send('Here is the list of all users.');
+});
+
+app.post('/users', (req, res) => {
+    res.send('A new user has been added.');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
