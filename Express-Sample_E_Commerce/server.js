dd1aa3e8cdd35126d22c32d8e8e86a5c2c98a1cb
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-08-11 01:31:07 +0530

    Added Code for Unknown Routes

---
+++ Express-Sample_E_Commerce/server.js
@@ -0,0 +1,34 @@
+/*
+Wildcard Route:
+Use a wildcard route (*) to handle all undefined routes and return the following custom error page as HTML:<h1>404 - Page Not Found</h1>
+Also add the status as 404.
+*/
+
+import express from 'express';
+
+const app = express();
+
+app.get('/products', (req, res) => {
+  res.send('Here is the list of all products.');
+});
+
+app.post('/products', (req, res) => {
+  res.send('A new product has been added.');
+});
+
+app.get('/categories', (req, res) => {
+  res.send('Here is the list of all categories.');
+});
+
+app.post('/categories', (req, res) => {
+  res.send('A new category has been created.');
+});
+
+app.use((req, res) => {
+// app.use('*',(req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
