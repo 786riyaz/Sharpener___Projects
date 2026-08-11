f9306b5fa7985310bdcb9503008889ac0cd1b0b8
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-08-11 23:42:02 +0530

    Added Code for Views Exercise of Form

---
+++ Express-View2/controllers/products.js
@@ -0,0 +1,14 @@
+import path from 'path';
+
+const serveFile = (req, res) => {
+  
+  // This will not work because we are using ES modules and __dirname is not defined in ES modules. We need to use path.resolve() to get the absolute path of the file.
+  // res.sendFile(path.join(__dirname, '../views/products.html'));
+  // res.sendFile(path.join(__dirname, "..","views","products.html"));
+  
+  // This will work because we are using path.resolve() to get the absolute path of the file.
+  // res.sendFile('products.html', { root: 'views' });
+  res.sendFile(path.resolve('VIEW', 'products.html'));
+}
+
+export { serveFile };
