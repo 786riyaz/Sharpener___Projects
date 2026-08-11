164b4704460bea823c37c267af353f814c2f30cb
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-08-11 18:21:22 +0530

    Added Code for E Commerce Express Server

---
+++ Express-E_Commerce/routes/cartRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  res.send(`Fetching cart for user with ID: ${userId}`);
+});
+
+router.post("/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  res.send(`Adding product to the cart for user with ID: ${userId}`);
+});
+
+export default router;
